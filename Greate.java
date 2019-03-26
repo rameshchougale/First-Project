828fcd89520b6c29eb04d007adcadb580f7a828e
rameshchougale <48961371+rameshchougale@users.noreply.github.com>
2019-03-26 23:41:06 +0530

    Finding greater no.
    Finding greater no.

---
+++ Greate.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class  Greate
+{
+	public static void main(String[] args) 
+	{
+			Scanner s=new Scanner(System.in);
+			int[] arr;
+			System.out.println("Enter no of element in list");
+			for(int i=0;i<no;i++)
+		{
+                s.arr[i].nextInt();  
+		}
+		for(int j=0;j<no;j++){
+          for(int k=i+1;k<arr[].length;k++)
+			{ 
+			   if(arr[j]>arr[k])
+				{
+                  Temp=arr[j];
+				  arr[j]=arr[k];
+				  arr[k]=arr[j];
+	 
+				}
+				System.out.println("min no is"+arr[0]);
+			}
+		}
+		}
+}
